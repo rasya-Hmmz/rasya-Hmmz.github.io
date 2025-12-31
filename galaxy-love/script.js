26b5054bf6d413c65cc511f0ff0e64c72fb229c5
rasya-Hmmz <grasya397@gmail.com>
2025-12-31 17:58:48 +0700

    Update script.js

--- galaxy-love/script.js
+++ galaxy-love/script.js
@@ -587,9 +587,9 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡I Like You♡",//untuk deskripsi planet layer 3
+  ' Galaxy of love From ...',//untuk deskripsi planet layer 1
+  " I love you ",//untuk deskripsi planet layer 2
+  "♡ I Like You ♡",//untuk deskripsi planet layer 3
   "31/12/2025",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
